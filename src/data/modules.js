8889596d5e7c46a89d418630dc5a0a6a92b2cd9e
Kmoon <hushan@kmoon.fun>
2025-11-29 14:43:28 +0800

    Update module data structure and add MOS.docx
    Expanded the components and outputs arrays for the first module in modules.js, adding new fields such as load, differential inductor, switching frequency, voltage ripple, and efficiency. Also updated output signals and units. Added a new document MOS.docx to the doc directory.

--- src/data/modules.js
+++ src/data/modules.js
@@ -5,16 +5,23 @@ export default [
     components: [
       { id: 1, name: '输入电压', signal: 'Vin', value: 0, unit: 'V' },
       { id: 2, name: '输出电压', signal: 'Vout', value: 0, unit: 'V' },
-      { id: 3, name: '额定功率', signal: 'Pout', value: 0, unit: 'W' }
+      { id: 3, name: '额定功率', signal: 'Pout', value: 0, unit: 'kW' },
+      { id: 4, name: '负载', signal: 'Rsingle', value: 0, unit: '' },
+      { id: 5, name: '差模电感', signal: 'Lin', value: 0, unit: '' },
+      { id: 6, name: '开关频率', signal: 'fres', value: 0, unit: 'Hz' },
+      { id: 7, name: '输入电压纹波', signal: 'VinRipple', value: 0, unit: 'V' },
+      { id: 8, name: '输出电压纹波', signal: 'VoutRipple', value: 0, unit: 'V' },
+      { id: 9, name: '效率', signal: 'ρ', value: 0, unit: '' }
     ],
     outputs: [
-      { id: 1, name: '结果1', signal: '符号1', value: 0, unit: '单位1' },
-      { id: 2, name: '结果2', signal: '符号2', value: 0, unit: '单位2' }
+      { id: 1, name: '结果1', signal: 'Psingle', value: 0, unit: '单位1' },
+      { id: 2, name: '输入电流', signal: 'Iin', value: 0, unit: 'A' },
+      { id: 3, name: '输出电流', signal: 'Iout', value: 0, unit: 'A' },
     ]
   },
   {
     moduleId: 2,
-    name: 'rc',
+    name: '测试',
     components: [
       { id: 1, name: '输入电压', signal: 'Vin', value: 0, unit: 'V' },
       { id: 2, name: '电阻', signal: 'R', value: 1000, unit: 'Ω' },
